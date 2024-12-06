0ac00904bb78a14356bb30f085725a3641b7497e
JesúsVidalLuyo <marilyn.vilcapuma.trujillo@vallegrande.edu.pe>
2024-12-06 08:51:57 -0500

    ...

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Reemplazar el valor de API_BACKEND en environment.ts
-sed -i "s|http://localhost:8080|${API_BACKEND}|g" /app/src/environments/environment.ts
-
-# Iniciar la aplicación
-npm start
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apigpt_fe: `${API_BACKEND}`
-};
